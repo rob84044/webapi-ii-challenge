ce9de9d0aa02755efe74a664329bd41316f7571d
RobertPedersenDev <rob84044@gmail.com>
2019-07-27 16:23:12 -0600

    MVP has been reached
    I had to change the file structure and renamed a file to get it to work. But it is now fully functional

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server')
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`server listening on port ${PORT}`)
 })
--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,6 +1,7 @@
-const postsRouter = require('express').Router
+const postsRouter = require('express').Router()
 const commentsRouter = require('../comments')
 const db = require('../../data/db')
+
 
 postsRouter.use('/:id/comments', commentsRouter)
 
